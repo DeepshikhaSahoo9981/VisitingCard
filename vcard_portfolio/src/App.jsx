1f127ed9e48bbb3ae321671e57555411be3ab8db
DeepshikhaSahoo9981 <deepshikha.sahoo.9981@gmail.com>
2026-07-15 01:30:01 +0530

    size configuration

--- vcard_portfolio/src/App.jsx
+++ vcard_portfolio/src/App.jsx
@@ -1,52 +1,73 @@
 import './App.css';
 import details from "./assets/Details.json";
 const profileImg = new URL(`./assets/${details.profilePhoto}`, import.meta.url).href;
-const visingCardImg = new URL(`./assets/${details.visitingCard}`, import.meta.url).href;
+const profileImg2 = new URL(`./assets/${details.secondaryProfile}`, import.meta.url).href;
 const resume = new URL(`./assets/${details.resume}`, import.meta.url).href;
 const visiting_card = new URL(`./assets/${details.addContacts}`, import.meta.url).href;
 import mailImg from "/icons/mail.svg";
 import callImg from "/icons/call.svg";
 import resumeImg from "/icons/download_resume.svg";
 import siteImg from "/icons/site.svg";
+import { useState } from 'react';
 
 
 function App() {
 
+  const [status, setStatus] = useState(false);
+
   return (
     <section className='Vcard'>
-      <div className='section-main'>
-        <div className='section-0'>
-          <img src={profileImg} alt="profile" />
-        </div>
-
-        <div className='section-1'>
-          <span className="name">{details.name}</span>
-          <span className="title">{details.title}</span>
-          <span className="description">{details.description}</span>
-        </div>
-
-        <div className='section-2'>
-          <a href={`tel:${details.mobileNo}`}>
-            <img src={callImg} alt="call" />
-          </a>
-
-          <a href={`mailto:${details.email}`}>
-            <img src={mailImg} alt="mail" />
-          </a>
-
-          <a href={details.website} target="_blank" rel="noopener noreferrer">
-            <img src={siteImg} alt="site" />
-          </a>
-
-          <a href={resume} download="Deepskikha_Resume.pdf">
-            <img src={resumeImg} alt="resume" />
-          </a>
-        </div>
-
-        <a href={visiting_card} download="Deepshikha_Contact.vcf" className='section-3'>
-          Add Contact
-        </a>
+      <div className='flip' onClick={() => setStatus(prev => !prev)}>
+        <div className={`flipBall ${status ? "open" : ""}`} onClick={() => setStatus(prev => !prev)}></div>
       </div>
+      {!status ?
+        <div className='section-main front'>
+          <div className='section-0'>
+            <img src={profileImg} alt="profile" />
+          </div>
+
+          <div className='section-1'>
+            <span className="name">{details.name}</span>
+            <span className="title">{details.title}</span>
+            <span className="description">{details.description}</span>
+          </div>
+
+          <div className='section-2'>
+            <a href={`tel:${details.mobileNo}`}>
+              <img src={callImg} alt="call" />
+            </a>
+
+            <a href={`mailto:${details.email}`}>
+              <img src={mailImg} alt="mail" />
+            </a>
+
+            <a href={details.website} target="_blank" rel="noopener noreferrer">
+              <img src={siteImg} alt="site" />
+            </a>
+
+            <a href={resume} download="Deepskikha_Resume.pdf">
+              <img src={resumeImg} alt="resume" />
+            </a>
+          </div>
+
+          <a href={visiting_card} download="Deepshikha_Contact.vcf" className='section-3'>
+            Add Contact
+          </a>
+        </div> :
+        <div className='section-main back'>
+          <div className='section-0'>
+            <img src={profileImg2} alt="profile" />
+          </div>
+
+          <div className='section-5'>
+            <span className="name">{details.name}</span>
+            <div className='info-section'>
+              <span className='emailId'>{details.email}</span>
+              <span className='siteConfig'>{details.website}</span>
+            </div>
+          </div>
+        </div>
+      }
     </section>
   )
 }
